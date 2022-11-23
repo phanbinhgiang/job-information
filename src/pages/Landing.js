c60747312db6d9a27cd1990b8d558f6c9a5fc9f1
Giang Phan <phanbinhgiang1999@gmail.com>
2022-11-23 19:32:10 +0700

    update

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -16,11 +16,6 @@ const Landing = () => {
           <h1>
             job <span>tracking</span> app
           </h1>
-          <p>
-            Crucifix narwhal street art asymmetrical, humblebrag tote bag pop-up
-            fixie raclette taxidermy craft beer. Brunch bitters synth, VHS
-            crucifix heirloom meggings bicycle rights.
-          </p>
           <Link to="/register" className="btn btn-hero">
             Login/Register
           </Link>
